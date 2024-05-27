6a863dc7bc492263c898dfe5b14f85905da9a7f3
Pai Khant Ko <kopaikhant@gmail.com>
2024-05-27 23:15:37 +0100

    create_tables_screen

--- lib/dimensions.dart
+++ lib/dimensions.dart
@@ -1,2 +1,7 @@
 //pass_screen.dart
 const double sbPasscodeWidth = 250;
+
+//tables_screen.dart
+const int gvTablesCrossAxisCount = 2;
+const double gvTablesAllAxisSpacing = 30.0;
+const double gvTablesMainAxisExtent = 300.0;
--- lib/views/tables_screen.dart
+++ lib/views/tables_screen.dart
@@ -1,12 +1,58 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:waiter_restaurant_pos/dimensions.dart';
+import 'dart:io' show Platform;
 
-class TablesScreen extends StatelessWidget {
+import 'package:waiter_restaurant_pos/views/menu_screen.dart';
+
+class TablesScreen extends StatefulWidget {
   const TablesScreen({super.key});
 
   @override
+  State<TablesScreen> createState() => _TablesScreenState();
+}
+
+class _TablesScreenState extends State<TablesScreen> {
+  final List<String> itemList = [
+    'Table 1',
+    'Table 2',
+    'Table 3',
+    'Table 4',
+    'Table 5',
+    'Table 6',
+    'Table 7',
+    'Table 8'
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Tables'),
+    return Scaffold(
+      body: Center(
+          child: GridView.builder(
+              shrinkWrap: true,
+              itemCount: itemList.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: gvTablesCrossAxisCount,
+                  mainAxisSpacing: gvTablesAllAxisSpacing,
+                  crossAxisSpacing: gvTablesAllAxisSpacing,
+                  mainAxisExtent: gvTablesMainAxisExtent),
+              itemBuilder: ((context, index) {
+                return Card(
+                  child: InkWell(
+                    onTap: () {
+                      const menuScreen = MenuScreen();
+                      Navigator.push(
+                          context,
+                          Platform.isIOS
+                              ? CupertinoPageRoute(
+                                  builder: (context) => menuScreen)
+                              : MaterialPageRoute(
+                                  builder: (context) => menuScreen));
+                    },
+                    child: Center(child: Text(itemList[index])),
+                  ),
+                );
+              }))),
     );
   }
 }
